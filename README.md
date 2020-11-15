78cf6a226e0dfd2ba78f0a49a5b7a02d68c9c904
Michael Rudden <michael@michaelrudden.com>
2020-11-15 07:07:14 -0500

    Testing out list generation with formatting

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # md2sh
- Convert markdown lists to shell script arrays
+Convert markdown lists to shell script arrays
+
+## How it works
+Reads every bullet point into an array following a header formatted with `###`
+
+## To Run
+Run `go run md2sh.go README.md` in your terminal
+
+### Test
+* Test 1
+* Test 2
+* Test 3
+
+This should produce a shell script array in stdout.
+
+### Test 2
+- Test 4
+- Test 5
+- Test 6
+
+### Test 3
++ Test 7
++ Test 8
++ Test 9
+
+### Test 4 (The Last One)
+* Bullet 1
+* Bullet 2
+* Bullet 3 (last bullet)
+
+This should be the end of the output.
---
+++ md2sh.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"log"
+	"strings"
+)
+
+//contents := []{"a", "b", "c"}
+
+//type List struct {
+//	title string
+//	contents []contents
+//}
+
+func main() {
+	fmt.Println("~~~ Here are your lists! ~~~\n")
+
+	inputFile := os.Args[1]
+		
+	file, err := os.Open(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Variable for formatting output
+	headerFound := false
+
+	// Scanner will read the file line by line
+	scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+
+			line := scanner.Text()
+			//fmt.Println(line)
+
+			//fmt.Println(strings.HasPrefix(scanner.Text(), "###"))
+
+			//skip any lines with length of 0
+			if len(line) > 0 {
+
+				// Remove any parenthesis and spaces to not break list
+				line = strings.Replace(line, "(", "", -1)
+				line = strings.Replace(line, ")", "", -1)
+				
+
+				if headerFound == false{
+
+					// If line starts with "###", create array from following unordered list
+					if strings.HasPrefix(line, "### ") {
+						//fmt.Println("!!!!!H3 Detected!!!!!")
+
+						// Replace spaces in header with underscores
+						line = strings.Replace(line, " ", "_", -1)
+						
+						header := strings.ToUpper(line[4:])
+						fmt.Println(header + "=(")
+						headerFound = true
+					}
+
+				} else {
+					if strings.Contains(line[0:2], "*") || strings.Contains(line[0:2], "-") || strings.Contains(line[0:2], "+") {
+						//fmt.Println("!!!!!List Item Detected!!!!!")
+
+						// Replace spaces in value with hyphens
+						line = strings.Replace(line, " ", "-", -1)
+
+						fmt.Println("  " + line[2:])
+					} //else {
+						//fmt.Println(")\n")
+						//headerFound = false
+					//}
+				}
+			} else {
+				if headerFound {
+					fmt.Println(")\n")
+				}
+				
+				headerFound = false
+			}
+		}
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+}
